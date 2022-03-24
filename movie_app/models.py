71222f31fc618b2559763140caca4ed2915d5b0f
dodir <mokronosovmw@gmail.com>
2022-03-24 17:26:41 +0500

    [upd] change Movie model

--- movie_app/models.py
+++ movie_app/models.py
@@ -7,3 +7,8 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=1_000_000)
+
+    def __str__(self):
+        return f'{self.name} - {self.rating}%'
