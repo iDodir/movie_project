ac7c4b1955a773a8947ae75370a062776fce9905
dodir <mokronosovmw@gmail.com>
2022-03-25 19:56:35 +0500

    [upd] add templates, slug, filters

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+from django.db.models import F, Sum, Max, Min, Count, Avg
+
 
 # Create your views here.
+
+def show_all_movies(request):
+    movies = Movie.objects.order_by(F('year').asc(nulls_last=True), '-rating')
+    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('id'))
+    return render(request, 'movie_app/all_movies.html', {
+        'movies': movies,
+        'agg': agg,
+        'total': movies.count(),
+    })
+
+
+def show_one_movie(request, slug_movie: str):
+    movie = get_object_or_404(Movie, slug=slug_movie)
+    return render(request, 'movie_app/one_movie.html', {
+        'movie': movie,
+    })
