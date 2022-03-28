1c8199458c74234a673611fe0136c0bee3322748
dodir <mokronosovmw@gmail.com>
2022-03-28 19:37:28 +0500

    [upd] admin setup

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -6,14 +6,42 @@ from django.db.models import QuerySet
 
 # Register your models here.
 
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', 'Низкий'),
+            ('от 40 до 59', 'Средний'),
+            ('от 60 до 79', 'Высокий'),
+            ('>=80', 'Высочайший'),
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<40':
+            return queryset.filter(rating__lt=40)
+        elif self.value() == 'от 40 до 59':
+            return queryset.filter(rating__gte=40).filter(rating__lt=60)
+        elif self.value() == 'от 60 до 79':
+            return queryset.filter(rating__gte=60).filter(rating__lt=80)
+        elif self.value() == '>=80':
+            return queryset.filter(rating__gte=80)
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
+    # fields = ['name', 'rating']
+    # exclude = ['slug']
+    # readonly_fields = ['year']
+    prepopulated_fields = {'slug': ('name',)}
     list_display = ['name', 'rating', 'currency', 'budget', 'rating_status']
     list_editable = ['rating', 'currency', 'budget']
     ordering = ['-rating', '-name']
     list_per_page = 10
     actions = ['set_dollars', 'set_euro']
     search_fields = ['name__startswith', 'rating']
+    list_filter = ['name', 'currency', RatingFilter]
 
     @admin.display(ordering='rating', description='Статус')
     def rating_status(self, movie: Movie):
